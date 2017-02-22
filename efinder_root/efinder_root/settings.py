c8019bf015752494872289c8958550c082719aa0
Katia Wu <katiayx@gmail.com>
2017-02-21 17:26:18 -0800

    configured postgresql for django-success

--- efinder_root/efinder_root/settings.py
+++ efinder_root/efinder_root/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'efinder_root.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'efinder',
+        'USER': 'katiayx',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
